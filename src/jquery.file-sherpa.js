b9f854fd4cf91b02affdc61ce6b5ed1c6b525c87
James Golick <james@giraffesoft.ca>
2009-05-28 22:44:48 -0400

    start making events work

--- src/jquery.file-sherpa.js
+++ src/jquery.file-sherpa.js
@@ -1,30 +1,36 @@
-function FileSherpaEventHandler(id, event) {
-  console.log(event.type);
-}
+var FileSherpa = {};
 
 (function($) {
-  var FileSherpa = function(element, options) {
-    this.options = $.extend({swfUrl: "/flash/uploader.swf",
-			     width:  250,
-			     height: 250}, options);
-    this.element = $(element);
+  $.extend(FileSherpa, {
+    widgets: {},
+    widget: function(element, options) {
+      this.options = $.extend({swfUrl: "/flash/uploader.swf",
+			       width:  250,
+			       height: 250}, options);
+      this.element = $(element);
 
-    var swf = new SWFObject(this.options.swfUrl,
-			    this.element.attr('id'),
-			    "100%",
-			    "100%",
-			    "9.0.0");
-    swf.addParam('wmode', 'transparent');
-    swf.addParam('allowScriptAccess', 'always');
-    swf.addParam('menu', 'false');
-    swf.addVariable('elementID', this.element.attr('id'));
-    swf.addVariable('eventHandler', 'FileSherpaEventHandler');
-    swf.write(this.element.attr('id'));
-  }
+      var swf = new SWFObject(this.options.swfUrl,
+			      this.element.attr('id'),
+			      "100%",
+			      "100%",
+			      "9.0.0");
+      swf.addParam('wmode', 'transparent');
+      swf.addParam('allowScriptAccess', 'always');
+      swf.addParam('menu', 'false');
+      swf.addVariable('elementID', this.element.attr('id'));
+      swf.addVariable('eventHandler', 'FileSherpa.eventHandler');
+      swf.write(this.element.attr('id'));
+
+      FileSherpa.widgets[this.element.attr('id')] = this;
+    },
+    eventHandler: function(id, event) {
+      FileSherpa.widgets[id].element.trigger(event);
+    }
+  });
 
   $.fn.fileSherpa = function(options) {
     $(this).each(function(i, element) {
-      new FileSherpa(element, options);
+      new FileSherpa.widget(element, options);
     });
   }
 }(jQuery));
